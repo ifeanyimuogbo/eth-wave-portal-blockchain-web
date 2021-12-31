6b071316e6fe443f439e2d9b752608f263556ff7
Ifeanyi Muogbo <doc3coder@gmail.com>
2021-12-31 03:05:20 +0100

    Change bio text

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import * as React from "react";
 import { ethers } from "ethers";
-import './App.css';
+import "./App.css";
 
 export default function App() {
+  const wave = () => {};
 
-  const wave = () => {
-    
-  }
-  
   return (
     <div className="mainContainer">
-
       <div className="dataContainer">
-        <div className="header">
-        👋 Hey there!
-        </div>
+        <div className="header">👋 Hey there!</div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+          I am Ifeanyi Muogbo and I'd be a terrific web3 dev so that's pretty
+          cool right? Connect your Ethereum wallet and wave at me!
         </div>
 
         <button className="waveButton" onClick={wave}>
